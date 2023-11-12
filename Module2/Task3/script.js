03cc786690af9f04d5886fa70a0e1e7d3066f536
Count X <marklinn@metropolia.fi>
2023-11-12 18:08:35 +0200

    Fixed module2 code

--- Module2/Task3/script.js
+++ Module2/Task3/script.js
@@ -2,9 +2,7 @@ const arr = [];
 let iput = "1";
 do {
   iput = prompt("Enter num");
-  arr.append(iput);
-}while(iput != "0")
-
-arr.sort();
-
-console.log(arr.toString());
+  if(iput !== "0" && iput) arr.push(iput);
+  else break;
+}while(true)
+console.log(arr.sort(function (a,b){return (b-a);}).toString());
